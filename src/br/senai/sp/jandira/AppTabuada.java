c187aa8c3840d5bf9cdcd06c39975e1ffc02e767
Yasmin Gonçalves <yasmin.coelho4@senaisp.edu.br>
2022-09-12 16:46:22 -0300

    Projeto finalizado

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,31 +1,17 @@
 package br.senai.sp.jandira;
 
 import br.senai.sp.jandira.gui.FrameTabuada;
-import br.senai.sp.jandira.model.*;
+
 public class AppTabuada {
 
 	public static void main(String[] args) {
 		
 		FrameTabuada tela = new FrameTabuada();
 		tela.altura = 600;
-		tela.largura = 470;
+		tela.largura = 455;
 		tela.titulo = "Tabuada";
 		tela.criarTela();
 		
-		Tabuada tabuada = new Tabuada();
-		
-		tabuada.multiplicando = 9;
-		tabuada.minMultiplicador = 8;
-		tabuada.maxMultiplicador = 10;
-		
-		String[] resultado = tabuada.getTabuada();
-		
-		int i = 0;
-		
-		while(i < resultado.length) {
-			System.out.println(resultado[i]);
-			i++;
-		}
 
 	}
 
--- src/br/senai/sp/jandira/gui/FrameTabuada.java
+++ src/br/senai/sp/jandira/gui/FrameTabuada.java
@@ -1,14 +1,24 @@
 package br.senai.sp.jandira.gui;
 
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+import br.senai.sp.jandira.model.Tabuada;
 
 public class FrameTabuada {
 
@@ -26,6 +36,7 @@ public class FrameTabuada {
 	public Color corBotaoCalcular = new Color(35, 150, 18);
 	public Color corBotaoLimpar = new Color(241, 207, 14);
 	
+
 	
 	public void criarTela() {
 		
@@ -41,15 +52,20 @@ public class FrameTabuada {
 		JLabel labelTitulo = new JLabel();
 		labelTitulo.setText("Tabuada 1.0");
 		labelTitulo.setFont(fonteTitulo);
-		labelTitulo.setBounds(60, 0, 300, 50);
+		labelTitulo.setBounds(70, 0, 300, 50);
 		labelTitulo.setForeground(corDoTitulo);
 		
+		ImageIcon icone = new ImageIcon("src/br/senai/sp/jandira/img/aluna.png");
+		JLabel icon = new JLabel(icone);
+		icon.setBounds(-15,  0, 100, 70);
 		
-		JTextArea labelSub = new JTextArea();
-		labelSub.setText("Com a tabuada 1.0 seus problemas acabaram. Calcule a tabuada que \ndesejar em segundos!");
-		labelSub.setFont(fonteSubtitulo);
-		labelSub.setBounds(60, 40, 400, 50);
-		labelSub.setForeground(corDoSubtitulo);
+		
+		JTextArea textAreaSub = new JTextArea();
+		textAreaSub.setText("Com a tabuada 1.0 seus problemas acabaram. Calcule a tabuada\nque desejar em segundos!");
+		textAreaSub.setFont(fonteSubtitulo);
+		textAreaSub.setBounds(70, 40, 400, 50);
+		textAreaSub.setForeground(corDoSubtitulo);
+		textAreaSub.setEditable(false);
 		
 		
 		JLabel labelMultiplicando = new JLabel();
@@ -60,6 +76,7 @@ public class FrameTabuada {
 		
 		JTextField textFieldMultiplicando = new JTextField();
 		textFieldMultiplicando.setBounds(250, 110, 150, 30);
+		textFieldMultiplicando.setHorizontalAlignment(textFieldMultiplicando.RIGHT);
 		
 		
 		JLabel labelMinMultiplicador = new JLabel();
@@ -70,6 +87,7 @@ public class FrameTabuada {
 		
 		JTextField textFieldMinMultiplicador = new JTextField();
 		textFieldMinMultiplicador.setBounds(250, 160, 150, 30);
+		textFieldMinMultiplicador.setHorizontalAlignment(textFieldMinMultiplicador.RIGHT);
 		
 		JLabel labelMaxMultiplicador = new JLabel();
 		labelMaxMultiplicador.setText("Máximo multiplicador:");
@@ -79,6 +97,7 @@ public class FrameTabuada {
 		
 		JTextField textFieldMaxMultiplicador = new JTextField();
 		textFieldMaxMultiplicador.setBounds(250, 210, 150, 30);
+		textFieldMaxMultiplicador.setHorizontalAlignment(textFieldMaxMultiplicador.RIGHT);
 		
 		JButton buttonCalcular = new JButton();
 		buttonCalcular.setText("Calcular");
@@ -98,10 +117,19 @@ public class FrameTabuada {
 		labelResultado.setBounds(40, 300, 150, 50);
 		
 		
+		
+		JList<String> lista = new JList<>();
+		
+		
+		JScrollPane scroll = new JScrollPane(lista);
+		scroll.setBounds(30, 350, 370, 200);
+		
+		
+		
 		tela.setVisible(true);
 		
 		painel.add(labelTitulo);
-		painel.add(labelSub);
+		painel.add(textAreaSub);
 		painel.add(labelMultiplicando);
 		painel.add(labelMinMultiplicador);
 		painel.add(labelMaxMultiplicador);
@@ -111,6 +139,62 @@ public class FrameTabuada {
 		painel.add(buttonCalcular);
 		painel.add(buttonLimpar);
 		painel.add(labelResultado);
+		painel.add(lista);
+		painel.add(scroll);
+		painel.add(icon);
+		
+		
+		
+		buttonCalcular.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if(textFieldMultiplicando.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Multiplicando vazio.");
+				}else if(textFieldMinMultiplicador.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Mínimo multiplicador vazio.");
+				}else if(textFieldMaxMultiplicador.getText().isEmpty()){
+					JOptionPane.showMessageDialog(null, "Máximo multiplicador vazio."); 
+				}else if(Integer.parseInt(textFieldMultiplicando.getText()) > 1000 || Integer.parseInt(textFieldMinMultiplicador.getText()) > 1000 || Integer.parseInt(textFieldMaxMultiplicador.getText()) > 1000){
+					JOptionPane.showMessageDialog(null, "Os valores devem ser maiores ou iguais à 1000.");
+				}else { 
+				
+					Tabuada tab = new Tabuada();
+					tab.multiplicando = Integer.parseInt(textFieldMultiplicando.getText());
+					tab.minMultiplicador = Integer.parseInt(textFieldMinMultiplicador.getText());
+					tab.maxMultiplicador = Integer.parseInt(textFieldMaxMultiplicador.getText());
+			
+					if(tab.maxMultiplicador < tab.minMultiplicador) {
+						JOptionPane.showMessageDialog(null, "Máximo multiplicador não pode ser maior que o mínimo.");
+					}
+								
+					lista.setListData(tab.getTabuada());
+				
+					scroll.getViewport().add(lista);
+								
+			}
+			}});
+		
+		
+		buttonLimpar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				textFieldMultiplicando.setText("");
+				textFieldMinMultiplicador.setText("");
+				textFieldMaxMultiplicador.setText("");
+			
+				DefaultListModel<String> listaL = new DefaultListModel<>();
+				lista.setModel(listaL);
+				
+				
+				
+			}
+		});
+		
+		
 		
 	}
 	
